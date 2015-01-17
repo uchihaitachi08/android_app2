0319784c9324e1c84ea4723a4f9fab1dc6e138ac
uchiha_itachi <sameerrrana@gmail.com>
2015-01-17 14:33:17 -0500

    finished local server databse

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		render 'new'
+  	else
+  		render 'new'
+  	end
+  end
+
+
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:train_no,:source,:destination,:doj,:clas,:limit,:auth_id)
+  end
 end
